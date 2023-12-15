d3d5a78142f26ce9c0c0396af12149d3eec1e58c
kavishka sandeepa <kavishkapk99@gmail.com>
2023-12-15 18:24:46 +0530

    PlaceOrder Done

--- src/main/java/Controller/PlaceOrderForm.java
+++ src/main/java/Controller/PlaceOrderForm.java
@@ -2,9 +2,11 @@ package Controller;
 
 import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import db.DBConnection;
 import dto.CustomerDto;
 import dto.ItemDto;
-import dto.tm.ItemTm;
+import dto.OrderDetailsDto;
+import dto.OrderDto;
 import dto.tm.OrderTm;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,16 +15,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.stage.Stage;
 import model.CustomerModel;
 import model.ItemModel;
+import model.OrderModel;
 import model.impl.CustomerImpl;
 import model.impl.ItemModelImpl;
+import model.impl.OrderModelImpl;
+
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlaceOrderForm {
@@ -36,6 +46,7 @@ public class PlaceOrderForm {
     public Button btnPlaceOrder;
     public JFXTreeTableView<OrderTm> treeTable;
     public Label lblTotalAmount;
+    public Label lblOrderId;
     @FXML
     private JFXTextField txtName;
     @FXML
@@ -54,10 +65,11 @@ public class PlaceOrderForm {
     private ItemModel itemModel;
     private List<CustomerDto> custlist;
     private List<ItemDto> itemlist;
-    private ObservableList<OrderTm> tmOrderList= FXCollections.observableArrayList();
-    private double totalAmount=0.00;
+    private ObservableList<OrderTm> tmOrderList = FXCollections.observableArrayList();
+    private double totalAmount = 0.00;
+    private OrderModel orderModel = new OrderModelImpl();
 
-    public void initialize(){
+    public void initialize() {
 
         colItemCode.setCellValueFactory(new TreeItemPropertyValueFactory<>("itemCode"));
         colDescription.setCellValueFactory(new TreeItemPropertyValueFactory<>("desc"));
@@ -65,37 +77,38 @@ public class PlaceOrderForm {
         colAmount.setCellValueFactory(new TreeItemPropertyValueFactory<>("amount"));
         colOption.setCellValueFactory(new TreeItemPropertyValueFactory<>("btn"));
 
-        customerModel=new CustomerImpl();
-        itemModel=new ItemModelImpl();
+        customerModel = new CustomerImpl();
+        itemModel = new ItemModelImpl();
         loadCustomerIds();
         loadItemeCodes();
         cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-            for (CustomerDto dto:custlist){
-                if(dto.getId().equals(newValue)){
+            for (CustomerDto dto : custlist) {
+                if (dto.getId().equals(newValue)) {
                     txtName.setText(dto.getName());
                     return;
-                };
+                }
+                ;
             }
         });
         cmbItemCode.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-            for (ItemDto dto:itemlist){
-                if(dto.getItemCode().equals(newValue)){
+            for (ItemDto dto : itemlist) {
+                if (dto.getItemCode().equals(newValue)) {
                     txtDesc.setText(dto.getDesc());
-                    txtUnitePrice.setText(String.format("%.2f",dto.getUnitePrice()));
+                    txtUnitePrice.setText(String.format("%.2f", dto.getUnitePrice()));
                     return;
-                };
+                }
+                ;
             }
         });
-
-
+        orderIdGenarate();
 
     }
 
     private void loadCustomerIds() {
         ObservableList<String> dto = FXCollections.observableArrayList();
         try {
-             custlist = customerModel.allCustomer();
-            for (CustomerDto x:custlist){
+            custlist = customerModel.allCustomer();
+            for (CustomerDto x : custlist) {
                 dto.add(x.getId());
             }
             cmbCustomerId.setItems(dto);
@@ -111,7 +124,7 @@ public class PlaceOrderForm {
         ObservableList<String> dto = FXCollections.observableArrayList();
         try {
             itemlist = itemModel.allItem();
-            for (ItemDto x:itemlist){
+            for (ItemDto x : itemlist) {
                 dto.add(x.getItemCode());
             }
             cmbItemCode.setItems(dto);
@@ -133,55 +146,56 @@ public class PlaceOrderForm {
         }
     }
 
-    public void addToCartButtonOnAction(ActionEvent actionEvent){
+    public void addToCartButtonOnAction(ActionEvent actionEvent) {
 
-        String id=cmbItemCode.getValue().toString();
+        String id = cmbItemCode.getValue().toString();
         try {
-            int qtyOnHand= itemModel.getItem(id).getQty();
+            int qtyOnHand = itemModel.getItem(id).getQty();
 
-            if(qtyOnHand<Integer.parseInt(txtQty.getText())){
-                new Alert(Alert.AlertType.ERROR,"Insuficent Quantity").show();
-                return;
-            } if (Integer.parseInt(txtQty.getText())<1) {
-                new Alert(Alert.AlertType.ERROR,"Input Valid Quantity").show();
+            if (qtyOnHand < Integer.parseInt(txtQty.getText())) {
+                new Alert(Alert.AlertType.ERROR, "Insuficent Quantity").show();
                 return;
             }
-            double amount=Integer.parseInt(txtQty.getText())*Double.parseDouble(txtUnitePrice.getText());
+            if (Integer.parseInt(txtQty.getText()) < 1) {
+                new Alert(Alert.AlertType.ERROR, "Input Valid Quantity").show();
+                return;
+            }
+            double amount = Integer.parseInt(txtQty.getText()) * Double.parseDouble(txtUnitePrice.getText());
 
-            JFXButton btn=new JFXButton("Delete");
-            OrderTm tm=new OrderTm(
-                  cmbItemCode.getValue().toString(),
-                  txtDesc.getText(),
-                  Integer.parseInt(txtQty.getText()),
-                  amount,
-                 btn
+            JFXButton btn = new JFXButton("Delete");
+            OrderTm tm = new OrderTm(
+                    cmbItemCode.getValue().toString(),
+                    txtDesc.getText(),
+                    Integer.parseInt(txtQty.getText()),
+                    amount,
+                    btn
             );
             btn.setOnAction((actionEvent1) -> {
                 tmOrderList.remove(tm);
-                totalAmount-=tm.getAmount();
-                lblTotalAmount.setText(totalAmount+"");
-            } );
-            boolean isExist= false;
+                totalAmount -= tm.getAmount();
+                lblTotalAmount.setText(totalAmount + "");
+            });
+            boolean isExist = false;
 
-            for (OrderTm orderTm:tmOrderList){
-                if (tm.getItemCode().equals(orderTm.getItemCode())){
-                    if (orderTm.getQty()+ tm.getQty()>qtyOnHand){
-                        new Alert(Alert.AlertType.ERROR,"Insuficent Quantity").show();
+            for (OrderTm orderTm : tmOrderList) {
+                if (tm.getItemCode().equals(orderTm.getItemCode())) {
+                    if (orderTm.getQty() + tm.getQty() > qtyOnHand) {
+                        new Alert(Alert.AlertType.ERROR, "Insuficent Quantity").show();
                         return;
                     }
-                    orderTm.setQty(orderTm.getQty()+ tm.getQty());//???????**************
-                    orderTm.setAmount(orderTm.getAmount()+ tm.getAmount());//?????**********
-                    isExist=true;
+                    orderTm.setQty(orderTm.getQty() + tm.getQty());//???????**************
+                    orderTm.setAmount(orderTm.getAmount() + tm.getAmount());//?????**********
+                    isExist = true;
 
                 }
             }
 
-            if (!isExist){
+            if (!isExist) {
                 tmOrderList.add(tm);
                 totalAmount += tm.getAmount();
             }
 
-            lblTotalAmount.setText(totalAmount+"");
+            lblTotalAmount.setText(totalAmount + "");
 
             TreeItem<OrderTm> treeItem = new RecursiveTreeItem<OrderTm>(tmOrderList, RecursiveTreeObject::getChildren);
             treeTable.setRoot(treeItem);
@@ -192,14 +206,64 @@ public class PlaceOrderForm {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
-        }catch (NumberFormatException exception){
-            new Alert(Alert.AlertType.ERROR,"Input Valid  Quantity").show();
+        } catch (NumberFormatException exception) {
+            new Alert(Alert.AlertType.ERROR, "Input Valid  Quantity").show();
+        }
+
+    }
+
+    public void orderIdGenarate() {
+        try {
+            OrderDto orderDto = orderModel.lastOrder();
+            if (orderDto == null) {
+                lblOrderId.setText("D001");
+            } else {
+                String string = orderDto.getOrderId();
+                int id = Integer.parseInt(string.split("D")[1]);
+                id++;
+                lblOrderId.setText(String.format("D%03d", id));
+            }
+
+        } catch (SQLException e) {
+            ;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
     }
 
 
-    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
-
-    }
+    public void placeOrderButtonOnAction(ActionEvent actionEvent){
+        List<OrderDetailsDto> list=new ArrayList<>();
+        for (OrderTm tm:tmOrderList){
+                list.add( new OrderDetailsDto(
+                        lblOrderId.getText(),
+                        tm.getItemCode(),
+                        tm.getQty(),
+                        tm.getAmount()/tm.getQty()
+                ));
+        }
+ //       if (!tmOrderList.isEmpty()) {
+            boolean isSaved = false;
+            try {
+                isSaved = orderModel.saveOrder(
+                        new OrderDto(
+                                lblOrderId.getText(),
+                                LocalDateTime.now().format(DateTimeFormatter.ofPattern("YY-MM-dd")),
+                                cmbCustomerId.getValue().toString(),
+                                list
+                        )
+                );
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            if(isSaved){
+                new Alert(Alert.AlertType.INFORMATION,"OrderSaved").show();
+            }
+     //   }
+        else { new Alert(Alert.AlertType.INFORMATION,"Some Error Here").show();}
+   }
 }
+
--- src/main/java/model/OrderModel.java
+++ src/main/java/model/OrderModel.java
@@ -1,6 +1,10 @@
 package model;
 
+import dto.OrderDto;
+
+import java.sql.SQLException;
+
 public interface OrderModel {
-
-
+    boolean saveOrder(OrderDto orderDto) throws SQLException, ClassNotFoundException;
+    OrderDto lastOrder() throws SQLException, ClassNotFoundException;
 }
